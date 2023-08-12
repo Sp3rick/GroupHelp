99e4c05ab57f7db80732722d4a94c64a6e3cd3c9
Sp3rick <62477178+Sp3rick@users.noreply.github.com>
2023-08-12 16:07:18 +0000

    Update utils.js

--- api/utils.js
+++ api/utils.js
@@ -7,10 +7,22 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function isValidChat(chat){
+
+    if ( !chat.hasOwnProperty("id") || !chat.hasOwnProperty("title") || !chat.hasOwnProperty("type")){
+
+        return false;
+
+    }
+    return true
+
+}
+
 module.exports = 
 {
 
     isNumber : isNumber,
     randomInt : randomInt,
+    isValidChat : isValidChat,
 
 }
