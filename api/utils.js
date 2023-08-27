84cab76e1c2995b70604b8e8950772220ba1ec44
Sp3rick <62477178+Sp3rick@users.noreply.github.com>
2023-08-27 13:01:16 +0000

    Update utils.js

--- api/utils.js
+++ api/utils.js
@@ -7,6 +7,56 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function parseCommand(text){
+
+    var prefix = text[0];
+    if( prefix == "/" || prefix == "!" || prefix == "." || prefix == "," || prefix == ";" ){
+
+        console.log( "Command prefix: " + prefix );
+
+        var temp = text.replace( prefix, "" );
+
+        var botCommand = temp.split(" ")[0]; //the entire start@usernamebot
+        var command = botCommand.split("@")[0];
+        var bot = botCommand.split("@")[1];
+
+        var args;
+        var splitArgs;
+        if( temp.split(" ").lentgh > 1)
+        {
+            args = temp.split(" ")[1];
+            splitArgs = args.split(" ");
+        }
+        else
+        {
+            args = false;
+            splitArgs = false;
+        }
+
+
+        var cmd = {
+            text : text,
+            prefix : prefix,
+            botCommand : botCommand,
+            command : command,
+            bot: bot,
+            args : args,
+            splitArgs : splitArgs,
+
+        }
+
+        return cmd;
+
+
+    }
+    else{
+
+        return false //is not a command
+
+    }
+
+}
+
 function isValidChat(chat){
 
     if ( !chat.hasOwnProperty("id") || !chat.hasOwnProperty("title") || !chat.hasOwnProperty("type")){
@@ -36,5 +86,6 @@ module.exports =
     randomInt : randomInt,
     isValidChat : isValidChat,
     isValidUser : isValidUser,
+    parseCommand : parseCommand
 
 }
